e356cdb0b446f8aa864d0403103b4005b8aac59b
abauer <abauer@jpiicardinals.org>
2013-05-22 12:02:21 -0500

    player stuffs

--- src/com/abauer/GuardGame/turn/AI.java
+++ src/com/abauer/GuardGame/turn/AI.java
@@ -2,8 +2,26 @@ package com.abauer.GuardGame.turn;
 
 public class AI extends Player{
 	
+	boolean done = false;
+	
 	public AI(){
 		super();
+	}
+	
+	public void takeTurn(int turn){
+		for(int index = 0; index<hand.size(); index++){
+			hand.get(index).setVisible(false);
+		}
+		if(turn == 1){
+			for(int index = 0; index<selected.length; index++){
+				selected[index] = hand.get(index);
+			}				
+		}
+		done = true;
+	}
+	
+	public boolean finished(int turn){
+		return (done&&TurnManager.followsRules(turn,selected));
 	}
 	
 }
