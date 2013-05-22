35b52aa97f319e9cf52711c55f71210ef39b9fb2
abauer <abauer@jpiicardinals.org>
2013-05-22 15:20:02 -0500

    still doesn't work

--- src/com/abauer/GuardGame/turn/AI.java
+++ src/com/abauer/GuardGame/turn/AI.java
@@ -13,7 +13,7 @@ public class AI extends Player{
 	}
 	
 	public void takeTurn(int turn){
-		delay(1000);
+//		delay(1000);
 		for(int index = 0; index<hand.size(); index++){
 			hand.get(index).setVisible(false);
 		}
